c348c1f67a801fe7521c46e57f8a2efc025d02df
isabel-nett <86269668+isabel-nett@users.noreply.github.com>
2023-02-04 18:09:02 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+const terminalOne = {
+                name:  'Ingenico Terminal',
+                id: '01',
+                image: 'src/images/terminal.png',
+                price: '$499',
+                connectivity: 'Ethernet 10/100 base T',
+                memory: '256 MB Flash, 128 MB RAM',
+                display: '2.8" backlight, QVGA (320x240 pixels)',
+                keypad: '15 keys, raised markings, backlit; 4 function keys',
+                magstripe: 'ISO1/2/3, 500K reads lifespan',
+                smartcard: 'EMV Level 1, 500K reads lifespan',
+                contactless: 'EMV Level 1 3.0 spec compliant',
+                camera: '2M pixels (QR code library embedded)',
+                security: 'PCI PTS 5.x & 6.x certified, rated for attended and self-service semi-attended use cases; Active connector removal; Kensington lock receptacles',
+                weight: '8.89 oz (254 g)',
+                size: '3.26" x 7.08" x 1.69" / 83 x 180 x 43 mm'
+            }
+
+const terminal2 = {
+                name: 'Mobile Wireless (Secure Android-Powered)',
+                id: '02',
+                image: 'src/images/QD3terminal.png',
+                price: '$399',
+                connectivity: '4G, WiFi, Bluetooth',
+                memory: '512MB RAM, 4GB Flash',
+                display: '4” color LCD with touch panel (800 x 480)',
+                keypad: 'Touchscreen',
+                magstripe: 'ISO 7811, Track 1/2/3, Bi-direction',
+                smartcard: 'ISO 7816, EMV Level 1 & Level 2',
+                contactless: 'ISO 14443 Type A & B, Mifare, contactless EMV Level 1, MasterCard Paypass, Visa PayWave, AE ExpressPay, Discover D-PAS',
+                camera: 'none',
+                security: 'PCI PTS 5.0',
+                weight: '200g',
+                size: '127 x 70 x 21mm',
+                battery: '3.7V, 3000mAh',
+                charging: '5V 1A adapter'
+            }
+            
+const terminal3 = {
+                name: 'Desktop Terminal (Secure Android-Powered)',
+                id: '03',
+                image: 'src/images/QD4terminal.png',
+                price: '$299',
+                connectivity: 'Ethernet, WiFi, USB',
+                memory: '1GB RAM, 8GB Flash',
+                display: '5.5” color LCD with touch panel (720 x 1280)',
+                keypad: 'Touchscreen',
+                magstripe: 'ISO 7811, Track 1/2/3, Bi-direction',
+                smartcard: 'ISO 7816, EMV Level 1 & Level 2',
+                contactless: 'ISO 14443 Type A & B, Mifare, Contactless EMV Level 1, MasterCard Paypass, Visa PayWave, ExpressPay and D-Pass (in progress)',
+                camera: '5M with auto-focus and flash',
+                security: 'PCI PTS 5.0',
+                weight: '400g',
+                size: '188 x 85 x 69mm'
+            }
