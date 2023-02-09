537c4becc7e9824d26e4252343503efb5484e089
isabel-nett <86269668+isabel-nett@users.noreply.github.com>
2023-02-09 09:27:26 -0600

    Completed cart functionality

--- app.js
+++ app.js
@@ -1,100 +1,83 @@
 const triple = new Triple('testapikey');
 
-triple.generatePaymentForm({
-	containerSelector: '#iFrame',
-	amount: 10.00,
+options = {
+   containerSelector: '#iFrame',
+   amount: 0.00,
     paymentOptions: ['credit_card'],
     tokenMode: false,
-	onError: (errorData) => {
-		console.log('Error');
-		console.log(errorData);
-	},
-	onSuccess: (data) => {
-		console.log('Success');
-		console.log(data);
-	},
-});
-
-
-triple.generatePaymentForm({
-	// ... Payment properties
-	styles: {
-		// button styling
-		button: {
-			// Color
-			color: '#fff',
-			// Backgroung
-			background: '#93e368',
-			// Border radius
-			borderRadius: '15px',
-			// Font size
-			fontSize: '18px',
-			// Handle :active pseudo class
-			active: {
-				// Change color on active state
-				color: '#eef7e9',
-				// Change background on active state
-				background: '#82cf59',
-			},
-			// Handle :hover pseudo class
-			hover: {
-				color: '#eef7e9',
-				background: '#8ed667',
-			},
-		},
-	},
-});
+    styles: {
+      // button styling
+      button: {
+         // Color
+         color: '#fff',
+         // Backgroung
+         background: '#93e368',
+         // Border radius
+         borderRadius: '15px',
+         // Font size
+         fontSize: '18px',
+         // Handle :active pseudo class
+         active: {
+            // Change color on active state
+            color: '#eef7e9',
+            // Change background on active state
+            background: '#82cf59',
+         },
+         // Handle :hover pseudo class
+         hover: {
+            color: '#eef7e9',
+            background: '#8ed667',
+         },
+      },
+   },
+   onError: (errorData) => {
+      console.log('Error');
+      console.log(errorData);
+   },
+   onSuccess: (data) => {
+      console.log('Success');
+      console.log(data);
+   },
+}
 
 // cart functionality
 
-const terminals = [
-        {
+const terminals = {
+        "qtyT1":{
             title: "Conventional Terminal",
             price: "499.00",
             qty: [0],
         },
-        {
+        "qtyT2":{
             title: "Mobile Wireless Terminal",
-            price: "499.00",
+            price: "269.00",
             qty:  [0],
         },
-        {
+        "qtyT3":{
             title: "Desktop Terminal",
-            price: "499.00",
+            price: "299.00",
             qty:  [0],
         },
-];
-
-function addToCart() {
-    if (terminals.qty > 0) {
-        var cartRow = document.createElement('div');
-        cartRow.classList.add('cart-row');
-        var cartRowContents = `
-        <div class="item-name cart-column">${title}</div>
-        <div class="item-quantity cart-column">${price}</div>
-        <div class="item-price cart-column">${qty * price}</div>
-        <button class="remove-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-x-circle" viewBox="0 0 16 16" id="removeItem">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-        </svg></button>`;
-        cartRow.innerHTML = cartRowContents;
-    };
 };
 
-// function addToCart(terminals, event) {
-//     event.preventDefault();
-//     forEach (terminal in terminals) {
-//         if (terminals.qty > 0) {
-//             var cartRow = document.createElement('div');
-//             cartRow.classList.add('cart-row');
-//             var cartRowContents = `
-//             <div class="item-name cart-column">${title}</div>
-//             <div class="item-quantity cart-column">${price}</div>
-//             <div class="item-price cart-column">${qty * price}</div>
-//             <button class="remove-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-x-circle" viewBox="0 0 16 16" id="removeItem">
-//                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-//                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-//             </svg></button>`;
-//             cartRow.innerHTML = cartRowContents;
-//         };
-// }};
+total = 0
+total_qty = 0
+_html = ''
+
+function addToCart() {
+    triple.clear();
+    total = 0;
+    total_qty = 0;
+    _html = '';
+    for (terminal in terminals) {
+        _t = document.getElementById(terminal);
+        total_qty = total_qty + parseInt(_t.value||0);
+        total = total + parseInt(_t.value||0) * parseFloat(terminals[terminal].price);
+        if (parseInt(_t.value||0) > 0) {
+            _html = _html+'<div class="cart-row"><div class="item-name cart-column">'+terminals[terminal].title+'</div><div class="item-quantity cart-column">'+parseInt(_t.value||0)+'</div><div class="item-price cart-column">$'+terminals[terminal].price * parseInt(_t.value||0)+'</div></div>'};
+        };
+    options.amount = total;
+    document.getElementById('cartItems').innerHTML= _html;
+    document.getElementById('totalPrice').innerHTML= `Total Price: $${total}`;
+    triple.generatePaymentForm(options);
+};
